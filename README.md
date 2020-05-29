853a1f2aa757158e03b845699886ccd8cc4e2b5d
Larry <lokyiuto.larry@gmail.com>
2020-05-29 13:45:49 +0800

    heroku

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # True or Drink
 
-This is an implementation of a famous drinking game called [True or Drink](https://www.playtruthordrink.com/). It will be so much easier for someone to use it on the phone rather than having a deck of card. 
+This is an implementation of a famous drinking game called [Truth or Drink](https://www.playtruthordrink.com/). It will be so much easier for someone to use it on the phone rather than having a deck of card. 
 
 ## Tech stack 
 - Flask for creating the web app 
 - Heroku for hosting the app 
-- SQLite for storing the question list 
+- Json for storing the question list 
 
+## Question list 
+I separated the question list into PG-13 and Rated-R, and the question list came from 
+- https://psycatgames.com/magazine/party-games/dirty-truth-or-dare/
 
+## New features idea 
 
--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 # Created on: 5/6/2020
 # A web app for True or Drink game which display questions on a card and users can click through it 
 
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, g
 from flask_sqlalchemy import SQLAlchemy 
 from datetime import datetime
+from random import randint 
+import json
+
 
 app = Flask(__name__)
 
@@ -14,9 +17,34 @@ def home():
 
 @app.route("/adult", methods = ["GET", "POST"])
 def adult():
+	
+
 	if request.method == "POST":
+		question = OptainQuestion("adult")
 		str = 'How would you rate your oral sex skills out of 10?'
-		return render_template("adult.html", content=str)
+		return render_template("adult.html", content=question)
+
+def normal():
+
+	if request.method == "POST":
+		question = OptainQuestion("normal")
+		str = 'How would you rate your oral sex skills out of 10?'
+		return render_template("adult.html", content=question)
+
+
+def OptainQuestion(gameName):
+	with open("Questions.json") as file:
+		questions = json.load(file)
+
+	if gameName == "adult": 
+		numQuestion = len(questions["Adult"]) - 1
+		questionNo = randint(0, numQuestion)
+		
+		return questions["Adult"][questionNo]
+
+	elif gameName == "normal":
+		questionNo = randint(0, len(questions["Normal"]))
+		return question["Normal"][questionNo]
 
 if __name__ == "__main__":
 	app.run(debug=True)
